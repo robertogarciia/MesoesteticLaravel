f156e8514338c8edadbf883e5f4f88ee5db33dc8
CristinaLobato <cristinals2283@gmail.com>
2024-04-26 20:21:52 +0200

    show arreglado

--- resources/views/showUpgrades.blade.php
+++ resources/views/showUpgrades.blade.php
@@ -11,7 +11,10 @@
         background-color: #f8f9fa;
         border-radius: 10px;
         padding: 20px;
+        max-width: 2500px; /* Ancho máximo del bg-modul */
+        margin: 0 auto; /* Margen automático para centrarlo horizontalmente */
     }
+
 
     .details {
         margin-bottom: 20px;
@@ -26,7 +29,16 @@
 </style>
 
 <div class="container">
-    <div class="bg-modul">
+    <div class="bg-modul"> 
+                
+        <div class="text-center"> 
+            @if($Upgrade->state === 'Valorandose')
+                <img src="{{ asset('images/candadoAbierto.png') }}" alt="Editable" style="width:30px;">
+            @else
+                <img src="{{ asset('images/candadoCerrado.png') }}" alt="No editable" style="width:30px;">        
+            @endif
+        </div>  
+
         <div class="d-flex justify-content-between align-items-center my-4">
             <h2>Datos de la mejora</h2>
             <div>
@@ -36,23 +48,25 @@
                 @endif
             </div>
         </div>
-        <div class="row">
+
+
+        <div class="row"> 
             <div class="col">
                 <div class="details">
                     <strong>Título:</strong>
-                    <div class="form-control editable" style="max-width: 400px; overflow-y: auto; max-height: 100px;">{{ $Upgrade->title }}</div>
+                    <div style="max-width: 400px; overflow-y: auto; max-height: 100px;">{{ $Upgrade->title }}</div>
                 </div>
                 <div class="details">
                     <strong>Estado:</strong>
-                    <div class="form-control editable" style="max-width: 400px; overflow-y: auto; max-height: 100px;">{{ $Upgrade->state }}</div>
+                    <div style="max-width: 400px; overflow-y: auto; max-height: 100px;">{{ $Upgrade->state }}</div>
                 </div>
                 <div class="details">
                     <strong>Likes:</strong>
-                    <div class="form-control editable" style="max-width: 400px; overflow-y: auto; max-height: 100px;">{{ $Upgrade->likes }}</div>
+                    <div style="max-width: 400px; overflow-y: auto; max-height: 100px;">{{ $Upgrade->likes }}</div>
                 </div>
                 <div class="details">
                     <strong>Zona:</strong>
-                    <div class="form-control editable" style="max-width: 400px; overflow-y: auto; max-height: 100px;">{{ $Upgrade->zone }}</div>
+                    <div style="max-width: 400px; overflow-y: auto; max-height: 100px;">{{ $Upgrade->zone }}</div>
                     <div class="circle" style="background-color: 
                         @switch($Upgrade->zone)
                             @case('Cosmeticos')
@@ -75,23 +89,32 @@
                 </div>
                 <div class="details">
                     <strong>Fecha de creación:</strong>
-                    <div class="form-control editable" style="max-width: 400px; overflow-y: auto; max-height: 100px;">{{ $Upgrade->created_at->format('d/m/Y') }}</div>
+                    <div style="max-width: 400px; overflow-y: auto; max-height: 100px;">{{ $Upgrade->created_at->format('d/m/Y') }}</div>
                 </div>
-            </div>
-            <div class="col">
+
                 <div class="mt-3">
                     <span class="d-block mb-1"><strong>Preocupación:</strong></span>
                     <div class="form-group">
-                        <textarea id="nota" name="nota" class="form-control mt-2 w-100" rows="10" disabled>{{ $Upgrade->worry }}</textarea>
+                        <div id="nota" name="nota"  rows="10" disabled>{{ $Upgrade->worry }}</div>
                     </div>
                 </div>
 
                 <div class="mt-3">
                     <span class="d-block mb-1"><strong>Beneficio:</strong></span>
                     <div class="form-group">
-                        <textarea id="nota" name="nota" class="form-control mt-2 w-100" rows="10" disabled>{{ $Upgrade->benefit }}</textarea>
-                    </div>
+                        <div id="nota" name="nota"  rows="10" disabled>{{ $Upgrade->benefit }}</div>
+                    </div>  
                 </div>
+
+            </div>
+            <div class="col">
+                @if($Upgrade->state === 'Valorandose')
+                    <img src="{{ asset('images/valorandose.png') }}" alt="Valorandose" style="width:230px;padding-right:10px;">
+                @elseif($Upgrade->state === 'En curso')
+                    <img src="{{ asset('images/enCurso.png') }}" alt="En Curso" style="width:230px;padding-right:10px;">
+                @elseif($Upgrade->state === 'Resuelta')
+                    <img src="{{ asset('images/resuelta.png') }}" alt="Resuelta" style="width:230px;padding-right:10px;">
+                @endif            
             </div>
         </div>
     </div>
